7929a4d07e66199995aceb3c1c445b9c44f48476
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-04-10 13:32:12 -0500

    completed ShoppingList app

---
+++ shopping-list/src/main/java/com/plurasight/ShoppingList.java
@@ -0,0 +1,22 @@
+package com.plurasight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("shirt");
+        System.out.println("socks");
+        System.out.println("juice");
+        System.out.println("tissue");
+        System.out.println("cereal");
+        System.out.println("apples");
+        System.out.println("strawberry");
+        System.out.println("beans");
+        System.out.println("kiwis");
+        System.out.println("beanie");
+        
+
+
+
+
+    }
+}
