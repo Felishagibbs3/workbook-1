8f42aba53f6338e1b6086cbf2e72af6dbae5cbe3
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-04-14 10:15:59 -0500

    completd mod3 exercise 2 - MathApp

--- MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -10,6 +10,7 @@ public class MathApp {
 
         System.out.println("The highest salary is " + highestSalary);
 
+        System.out.println("---------------------------------");
 
         {
             int carPrice = 32000;
@@ -17,7 +18,43 @@ public class MathApp {
             int lowestPrice = Math.min(carPrice, truckPrice);
             System.out.println("The lowest price is " + truckPrice);
 
+            System.out.println("---------------------------------");
+
         }
+            double cirRadius = 7.25;
+            double area = Math.PI * Math.pow(cirRadius, 2);
+
+            System.out.println("The area of the cirlce with a radius of " + cirRadius + "is:" + area);
+
+            System.out.println("---------------------------------");
+
+            double number = 5.0;
+            double squareRoot = Math.sqrt(number);
+            System.out.println("The square root of " + number + "is:" + squareRoot);
+
+            System.out.println("---------------------------------");
+
+            int x1 = 5;
+            int x2 = 85;
+            int y1 = 10;
+            int y2 = 50;
+
+            double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2-y1, 2));
+
+            System.out.println("The distance is: " + distance);
+
+            System.out.println("---------------------------------");
+
+
+            double x = -3.8;
+            double absValue = Math.abs(x);
+
+            System.out.println("The absolute value of " + x + "is:" + absValue);
+
+            System.out.println("---------------------------------");
+
+
+            System.out.println("A random number between 0 and 1 is: " + Math.random());
 
 
 
