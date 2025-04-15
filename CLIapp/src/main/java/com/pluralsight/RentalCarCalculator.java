f1952f65cf911985f9cd240ce235d7eafbb05c72
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-04-15 17:04:35 -0500

    Completed Rental Car Calculator

--- CLIapp/src/main/java/com/pluralsight/RentalCarCalculator.java
+++ CLIapp/src/main/java/com/pluralsight/RentalCarCalculator.java
@@ -1,4 +1,75 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class RentalCarCalculator {
-}
+    static Scanner theScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+        // ask user for a date and store it as a string
+        System.out.println("What is the pick-up date?");
+        String date = theScanner.nextLine();
+
+        // ask user for number of days for rental
+        //store as int
+        System.out.println("How many days are you renting for?");
+        int day = theScanner.nextInt();
+
+        theScanner.nextLine();
+
+        //do you want an electronic toll tag? 3.95 upcharge per day
+        // store answer as a string
+        System.out.println("Toll tag? (3.95/per day) (y/n):");
+        String tollTag = theScanner.nextLine();
+
+        // do you want gps? 2.95 per day
+        System.out.println("GPS? (2.95/per day) (y/n)");
+        String gps = theScanner.nextLine();
+
+        //do you want roadside assit for 3.95 per day
+        System.out.println("Roadside assistance? (3.95/per day) (y/n)");
+        String roadSide = theScanner.nextLine();
+
+        // need to ask their age
+        System.out.println("How old are you?");
+        int age = theScanner.nextInt();
+
+
+        //calculate base rental price 29.99 per day
+        double basePrice = 29.99 * day;
+
+        // figure out options cost (gps + toll + roadside)
+        double optionsCost = 0;
+        if (tollTag.equals("y")) {
+            optionsCost += 3.95;
+
+        }
+        if (gps.equals("y")) {
+            optionsCost += 2.95;
+
+        }
+        if (roadSide.equals("y")) {
+            optionsCost += 3.95;
+
+        }
+
+        optionsCost *= day;
+
+        double subTotal = basePrice + optionsCost;
+
+        double surcharge = 0;
+        if (age< 25) {
+            surcharge = subTotal * .3;
+
+        double total = subTotal + surcharge;
+
+        System.out.printf("Base Rental Cost $%.2f%n", basePrice);
+        System.out.printf("Options Cost $%.2f%n", basePrice);
+        System.out.printf("Underage surcharge $%.2f%n", basePrice);
+        System.out.printf("Cost $%.2f%n",total);
+
+        }
+        }
+    }
--- SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
+++ SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -1,4 +1,80 @@
 package com.pluralsight;
 
+
+import java.util.Scanner;
+
 public class SandwichShop {
+
+    static Scanner myScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean appRunning = true;
+        while (appRunning == true) {
+
+            int SandwichSize = sandwichSizeSelection();
+
+            double total = 0;
+            if (SandwichSize == 1) {
+                total += 5.45;
+            } else if (SandwichSize == 2) {
+                total += 8.95;
+            } else {
+                System.out.println("\nInvalid Selection, try again.\n");
+                return;
+            }
+
+            int discount = 0;
+            try {
+
+                int customerAge = Age();
+                if (customerAge <= 17) {
+                    discount = 10;
+                } else if (customerAge >= 65) {
+                    discount = 20;
+                }
+
+            } catch (Exception e) {
+                System.out.println("\nInvalid age. Start over\n");
+                myScanner.nextLine();
+                return;
+            }
+
+            total = total - (total * discount / 100);
+            displayTotal(SandwichSize, total, discount);
+
+
+            System.out.println("What size sandwich would you like?");
+            System.out.println("1: Regular : 5.45");
+            System.out.println("2: Large : 8.95");
+
+            int userchoice = myScanner.nextInt();
+            int AgeSection;
+            System.out.println("How old are you?");
+            System.out.println(" <= 17 gets 10% off");
+            System.out.println(">= 65 gets 20% off");
+            System.out.println("Enter your age:");
+
+            int age = myScanner.nextInt();
+
+            String size = "";
+            if (SandwichSize == 1) {
+                size = "Regular";
+            } else if (SandwichSize == 2) {
+                size = "Large";
+
+                if (discount > 0) {
+                    System.out.println("This includes your " + discount + "% discount");
+                }
+
+            }
+        }
+
+
+    }
+
+
 }
+
+
+
+
